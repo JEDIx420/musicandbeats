21333c5a887c2e7242836363586e521deb40f141
Jayanand J <117061161+JEDIx420@users.noreply.github.com>
2026-08-21 01:20:45 +0530

    Bump PWA cache for V4 refinements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'musicandbeats-v3.1';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./workflow-fixes.js','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'musicandbeats-v4';
+const ASSETS = ['./','./index.html','./styles.css','./v4-refinements.css','./app.js','./workflow-fixes.js','./v4-refinements.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
